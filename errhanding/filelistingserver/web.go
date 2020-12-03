4327713abbbc42afb73846ec8807487728f71c4b
Ricardo <ricardo.luo@icloud.com>
2020-12-03 13:35:33 +0800

    feat: httptest

--- errhanding/filelistingserver/web.go
+++ errhanding/filelistingserver/web.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"go-demo/errhanding/filelistingserver/fileListing"
+	"log"
 	"net/http"
 	"os"
 )
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
+type userError interface {
+	error
+	Message() string
+}
+
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Printf("Panic: %v", r)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
-			//log.Warn("Error handling request: %s", err.Error())
+			log.Printf("Error handling request: %s", err.Error())
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -28,7 +44,7 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/list/", errWrapper(fileListing.HandleFileList))
+	http.HandleFunc("/", errWrapper(fileListing.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
